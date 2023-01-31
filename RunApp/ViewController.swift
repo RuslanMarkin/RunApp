2c5c853d328c38d2806c5151accc1199096a4ae9
RuslanMarkin <ruslan.markin.work@gmail.com>
2023-01-31 05:14:57 +0300

    Update map view

--- RunApp/ViewController.swift
+++ RunApp/ViewController.swift
@@ -7,39 +7,55 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
+    
+    var locationManager: CLLocationManager!
+    var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+        
+        mapView = MKMapView()
+        self.view.addSubview(mapView)
+        
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+        mapView.topAnchor.constraint(equalTo: self.view.topAnchor),
+        mapView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+        mapView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+        mapView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
+        ])
+        
+        mapView.showsUserLocation = true
+        locationManager = CLLocationManager()
+        locationManager.requestWhenInUseAuthorization()
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
         
-        
-        let mapView = MKMapView()
-        
-        let leftMargin:CGFloat = 6
-        let topMargin:CGFloat = 60
-        let mapWidth:CGFloat = view.frame.size.width-20
-        let mapHeight:CGFloat = 300
-        
-        mapView.frame = CGRect(x: leftMargin, y: topMargin, width: mapWidth, height: mapHeight)
-        
-        
-        mapView.mapType = MKMapType.standard
-        mapView.isZoomEnabled = true
-        mapView.isScrollEnabled = true
-             
-             // Or, if needed, we can position map in the center of the view
-        mapView.center = view.center
-             
-        
-        view.addSubview(mapView)
-
     }
+//
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//
+//
+//        let mapView = MKMapView()
+//
+//        let leftMargin:CGFloat = 60
+//        let topMargin:CGFloat = 60
+//        let mapWidth:CGFloat = view.frame.size.width
+//        let mapHeight:CGFloat = 870
+//        mapView.frame = CGRect(x: leftMargin, y: topMargin, width: mapWidth, height: mapHeight)
+//
+//
+//        mapView.mapType = MKMapType.standard
+//        mapView.isZoomEnabled = true
+//        mapView.isScrollEnabled = true
+//             // Or, if needed, we can position map in the center of the view
+//        mapView.center = view.center
+//        view.addSubview(mapView)
+
     
 
 
